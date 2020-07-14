69ac59f60e8cf379060237aad8af712fa0239fe6
Mariia Cherniuk <marydort1@gmail.com>
2020-07-14 17:00:45 +0100

    TextEditor

---
+++ NanoGO/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  NanoGO
+//
+//  Created by Mariia Cherniuk on 14/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var text = ""
+    
+    var body: some View {
+        NavigationView {
+            TextEditor(text: $text)
+                .foregroundColor(.red)
+                .background(Color.blue) //doesn't work
+                .font(.body)
+                .lineSpacing(5)
+                .navigationTitle("TextEditor")
+        }
+        .onAppear {
+            //how to fix .background(Color.blue)
+            UITextView.appearance().backgroundColor = .clear
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
